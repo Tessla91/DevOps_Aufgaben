49c390c91a78967f4e44b3e3b9aadd1c74e5bb04
Tessa <160845611+Tessla91@users.noreply.github.com>
2024-05-28 11:31:07 +0200

    chapter 2

--- src/model_b-1.py
+++ src/model_b-1.py
@@ -5,11 +5,11 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 # warnings.filterwarnings("ignore")
 
 # evaluate a model
-def evaluate_model(X, y, model):
+def evaluate_model(x, y, model):
 # define evaluation procedure
     cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=10)
     # evaluate model
-    scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1)
+    scores = cross_val_score(model, x, y, scoring='accuracy', cv=cv, n_jobs=-1)
     return scores
 
 
